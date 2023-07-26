e297800c754d2112d83cf35338d77d6b16c3ba03
Nolra <nolraprof@gmail.com>
2023-07-26 13:06:23 +0000

    Deploying to public from @ Nolra/next_intro_hw@18751bf1263e622721e3f31754f3a943db9c4319 🚀

--- _next/static/n7XDzODFzMywvsp5oOVg9/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/14a264d028709040.css","static/chunks/pages/index-9261dca2dc3c36c9.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();

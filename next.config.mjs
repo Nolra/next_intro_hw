68e081c7aec98aed68fbc51b60c9583d26f6a91f
nolraprof@gmail.com <nolraprof@gmail.com>
2023-07-29 21:07:56 +0400

    add new hw part

--- next.config.mjs
+++ next.config.mjs
@@ -4,10 +4,12 @@
 
 const nextConfig = process.env.NODE_ENV === 'development' ? {
   reactStrictMode: true,
+  basePath: '',
   images: {
     domains: ['via.placeholder.com'],
   },
 } : {
+  basePath: 'next_intro_hw/',
   images: {
     loader: 'akamai',
     path: '',

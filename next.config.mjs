a76ff8cf4a2373bda6a7daf8a1fd230abb4d3a9a
nolraprof@gmail.com <nolraprof@gmail.com>
2023-07-29 20:36:45 +0400

    add new hw part

--- next.config.mjs
+++ next.config.mjs
@@ -2,22 +2,20 @@
  * @type {import('next').NextConfig}
  */
 const isProd = process.env.NODE_ENV === 'prod'
-const nextProdConfig = {
+const nextConfig = isProd ? {
   images: {
     loader: 'akamai',
     path: '',
     domains: ['via.placeholder.com'],
   },
   assetPrefix: 'https://nolra.github.io/next_intro_hw',
+} : {
+  reactStrictMode: true,
+  images: {
+    domains: ['via.placeholder.com'],
+  },
 };
 
-const nextLocalConfig = {
-    reactStrictMode: true,
-    images: {
-      domains: ['via.placeholder.com'],
-    },
-};
 
-
-export default isProd ? nextProdConfig : nextLocalConfig;
+export default nextConfig;
  

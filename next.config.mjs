f8321f5e358f18135dce32d3e1cbf89ab7a35473
nolraprof@gmail.com <nolraprof@gmail.com>
2023-07-30 17:34:37 +0400

    add new hw part

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,30 @@
+// const nextConfig = process.env.NODE_ENV === 'production' ? {
+//   // basePath: '/next_intro_hw',
+//   assetPrefix: 'https://nolra.github.io/next_intro_hw',
+//   images: {
+//     loader: 'akamai',
+//     path: '',
+//     // domains: ['via.placeholder.com'],
+//   },
+// } : {
+//   reactStrictMode: true,
+//   images: {
+//     domains: ['via.placeholder.com'],
+//   },
+// };
+
+// export default nextConfig;
+ 
 /**
  * @type {import('next').NextConfig}
  */
-
-const nextConfig = process.env.NODE_ENV === 'production' ? {
-  // basePath: '/next_intro_hw',
-  assetPrefix: 'https://nolra.github.io/next_intro_hw',
+const isProd = process.env.NODE_ENV === 'production'
+const nextConfig = {
   images: {
     loader: 'akamai',
     path: '',
-    // domains: ['via.placeholder.com'],
   },
-} : {
-  reactStrictMode: true,
-  images: {
-    domains: ['via.placeholder.com'],
-  },
+  assetPrefix: isProd ? 'https://nolra.github.io/next_deploy' : undefined,
 };
 
-
 export default nextConfig;
- 
